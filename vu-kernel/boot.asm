1316ad19dd67eeedbc11d59b47b70752f09e3a68
Kuba Clark <jakub.clark@protonmail.com>
2019-04-22 20:16:07 +0200

    Remove multiboot2.h

--- vu-kernel/boot.asm
+++ vu-kernel/boot.asm
@@ -1,36 +1,32 @@
-; Copyright (C) 2014  Arjun Sreedharan
-; License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
-
 bits 32
 section .text
-        ;multiboot spec
         align 4
-        dd 0x1BADB002              ;magic
-        dd 0x00                    ;flags
-        dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
+        dd 0x1BADB002
+        dd 0x00
+        dd - (0x1BADB002 + 0x00)
 
 global start
 global keyboard_handler
 global load_idt
 
-extern Kernel_Main 		;this is defined in the c file
+extern Kernel_Main
 extern keyboard_handler_main
 
 load_idt:
 	mov edx, [esp + 4]
 	lidt [edx]
-	sti 				;turn on interrupts
+	sti
 	ret
 
 keyboard_handler:                 
-	call    keyboard_handler_main
+	call keyboard_handler_main
 	iretd
 
 start:
-	cli 				;block interrupts
+	cli
 	mov esp, stack_space
 	call Kernel_Main
-	hlt 				;halt the CPU
+	hlt
 
 section .bss
 resb 8192; 8KB for stack
